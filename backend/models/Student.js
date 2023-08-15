b62b79a22d32ad4b06cd5f76bc26bbd425c99506
Naveed Ahmed <76599103+NaveedAhmed55@users.noreply.github.com>
2023-08-16 03:11:29 +0500

    deleted a file

--- backend/models/Student.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose')
-
-const studentSchema=new mongoose.Schema({
-    username:{type:String,required:true},
-    password:{type:String,required:true},
-    role:{type:String,required:true}
-})
